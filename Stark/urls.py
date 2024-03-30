450f971b145500e35c2633b3d38b8e735d6d462a
EC2 Default User <ec2-user@ip-172-31-38-11.eu-west-1.compute.internal>
2024-03-30 22:20:54 +0000

    Made search and name filter for application

--- Stark/urls.py
+++ Stark/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import frontpage
+from product.views import product
+from core.views import frontpage, shop
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('shop/', shop, name='shop'),
+    path('product/', product, name='product/'),
     path('admin/', admin.site.urls),
 ]
