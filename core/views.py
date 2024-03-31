f21a29bd6efcdd48a9a71ca5e93e95606c9e7977
EC2 Default User <ec2-user@ip-172-31-38-11.eu-west-1.compute.internal>
2024-03-31 14:09:18 +0000

    Changes for website

--- core/views.py
+++ core/views.py
@@ -2,28 +2,31 @@ from django.db.models import Q
 from django.shortcuts import render
 from product.models import Product, Category
 
+def get_products(category_slug=None, query=None):
+    products = Product.objects.all()
+    if category_slug:
+        products = products.filter(category__slug=category_slug)
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    return products
+
 def frontpage(request):
-    products = Product.objects.all()[0:15]
+    # Consider pagination here
+    products = Product.objects.all()[:15]
     return render(request, 'core/frontpage.html', {'products': products})
 
 def shop(request):
     categories = Category.objects.all()
-    products = Product.objects.all()
-    
     active_category = request.GET.get('category', '')
-    
-    if active_category:
-        products = products.filter(category__slug=active_category)
-    
-    query= request.GET.get('query', '')
-    
-    if query:
-        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
-    
+    query = request.GET.get('query', '')
+
+    products = get_products(category_slug=active_category, query=query)
+
     context = {
         'categories': categories,
-        'products' : products,
-        'active_category': active_category
+        'products': products,
+        'active_category': active_category,
+        'query': query
     }
-    
+
     return render(request, 'core/shop.html', context)
