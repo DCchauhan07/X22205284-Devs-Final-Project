0f7dbff7e4e89dfc73200cdde1616bc935761d23
EC2 Default User <ec2-user@ip-172-31-38-11.eu-west-1.compute.internal>
2024-04-02 00:45:53 +0000

    Changes made for cart and website

--- Stark/urls.py
+++ Stark/urls.py
@@ -1,37 +1,11 @@
-"""Stark URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.contrib.auth import views
 from django.conf.urls.static import static
 
-from cart.views import add_to_cart, cart, checkout
-from product.views import product
-from core.views import frontpage, shop, signup
-
 urlpatterns = [
-    path('', frontpage, name='frontpage'),
-    path('shop/', shop, name='shop'),
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('shop/<slug:slug>/', product, name='product'),
+    path('', include('core.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
-    path('cart/', cart, name='cart'),
-    path('cart/checkout', checkout, name='checkout'),
-    path('signup/', signup, name='signup'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/views.py
+++ product/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseServerError
 
 from .models import Product
 
-# Create your views here.
 def product(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    
-    return render(request, 'product/product.html', {'product': product})
+    try:
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'product/product.html', {'product': product})
+    except Product.MultipleObjectsReturned:
+        return HttpResponseServerError("Product doesn't exist")
